7a6d1f81a30849bb91c6adec980e9277a9cf882c
heyitsmerahulmishraa-arch <heyitsmerahulmishraa@gmail.com>
2025-10-03 12:20:53 +0530

    02-10-2025

---
+++ 02-10-2025/script.js
@@ -0,0 +1,138 @@
+// immutable 
+// let demo = "hello"
+// demo = "world"
+// console.log(demo)
+
+// let obj = {name:"mark"}
+
+// let demoObj = {class:"5th", country:"India"}
+
+// let obj2 = obj
+// let obj2 = {...obj}
+// let obj2 = {...demoObj}
+// let obj2 = Object.assign({},obj)
+// let obj2 = Object.assign(demoObj,obj)
+
+// obj2.name = "john"
+
+// console.log(obj)
+// console.log(obj2)
+
+
+// let a = undefined
+// let b = null
+
+// console.log(typeof (a))
+// console.log(typeof (b))
+
+// if(b === null){
+//     console.log("equal to")
+// }else{
+//     console.log("not equal to")
+// }
+
+
+// let x = 5 > 4 == true != 0
+// let x2 = 5 > 3 > 1 > 0
+
+// console.log(x)
+// console.log(x2)
+
+// let a = ["ab"]
+// let b = ["ab"]
+
+// console.log(a === b)
+
+// console.log("10" < "9")
+// let a = "23"
+// let b = "4"
+
+// console.log(typeof(a))
+// console.log(typeof(b))
+
+// console.log(a == b)
+
+// console.log(undefined > 0)
+// console.log("1" > 0)
+// console.log("0"> 0)
+
+// let arr1 = [1,2,3,4]
+// let arr2 = [1,2,3,4]
+
+// console.log(arr1 == arr2)
+// console.log(arr1[0] == arr2[0])
+
+// let hugeNum = "999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999"
+
+// console.log(hugeNum.length)
+// console.log(typeof hugeNum)
+
+// console.log(Infinity > 1000)
+// console.log(Infinity > 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999)
+
+// console.log("10"+ 5)
+// console.log(10 + "5")
+// console.log("10" + "5")
+// console.log("10" - 5)
+// console.log("10" - "5")
+// console.log("10" - "5j")
+
+// let a = 5
+// b = a++
+
+// console.log(a)
+// console.log(b)
+
+// let x = 3
+// y = ++x
+
+// console.log(x)
+// console.log(y)
+
+// let x = 3
+// y = x++
+
+// console.log(x)
+// console.log(y)
+
+// let a = "4"
+// let b = -a
+// let c = -true
+// let d = -false
+// let e = -null
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+
+// let a = "4"
+// let b = +a
+// let c = +true
+// let d = +null
+// let e = +false
+// let f = +[]
+// let g = +{}
+// let h = +undefined
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+
+// let a = 2
+// const b = "3"
+
+// console.log(a)
+// console.log(a += b)
+
+// console.log(2/0)
+
+let x = 5
+console.log(x <<= 4)
+
